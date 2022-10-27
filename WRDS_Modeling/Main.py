cfc670f68691720f113006f9db6cd73371a4fd89
JoshuaOldmixon <97045114+JoshuaOldmixon@users.noreply.github.com>
2022-10-26 23:33:56 -0400

    Update Main.py

--- WRDS_Modeling/Main.py
+++ WRDS_Modeling/Main.py
@@ -1,7 +1,6 @@
 from Tools.Research import wrdsData
 from Tools.Models.LinearRegression import linearRegression
 from Tools.Models.LGBMRegressor import lightGBMRegressor
-#from Tools.Models.H2OXGBoost import H2OXGBoost
 from datetime import date
 from dateutil.relativedelta import relativedelta
 
@@ -9,10 +8,9 @@ from dateutil.relativedelta import relativedelta
 start = (date.today()- relativedelta(years=1)).strftime("%m/%d/%Y")
 
 data = wrdsData('AAPL',start).table
+
 linReg = linearRegression(data)
-#h2oboost = H2OXGBoost(data)
 lgbmreg = lightGBMRegressor(data)
 
 print ('Linear Regression:',linReg.score())
-#print ('H2O XGBoost:',h2oboost.score())
 print ('Light GBM Regressor:',lgbmreg.score())
